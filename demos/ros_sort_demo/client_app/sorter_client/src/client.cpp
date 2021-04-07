d9ac347a6643366fe8241c0c1805f5340ec83af4
Christian Lienen <christianlienen@me.com>
2021-04-07 10:53:59 +0200

    small improvements on the demo client app

--- demos/ros_sort_demo/client_app/sorter_client/src/client.cpp
+++ demos/ros_sort_demo/client_app/sorter_client/src/client.cpp
@@ -11,7 +11,7 @@ using namespace std::chrono_literals;
 class ClientNode : public rclcpp::Node
 {
   public:
-    ClientNode() : Node("minimal_publisher"), count_(0)
+    ClientNode() : Node("SorterClientNode"), count_(0)
     {
       publisher_ = this->create_publisher<sorter_msgs::msg::Sortdata>("unsorted", 10);
       timer_ = this->create_wall_timer(2s, std::bind(&ClientNode::timer_callback, this));
@@ -27,17 +27,14 @@ class ClientNode : public rclcpp::Node
       message.sortdata.resize(2048);
       std::generate(message.sortdata.begin(), message.sortdata.end(), std::rand);
 
-      
-      
-      RCLCPP_INFO(this->get_logger(), "Publishing, Cnt='%d'", cnt);
+      RCLCPP_INFO(this->get_logger(), "Unsorted data published, Cnt='%d'", cnt++);
       publisher_->publish(message);
     }
 
 
 	  void topic_callback(const sorter_msgs::msg::Sortdata::SharedPtr msg) const
     {
-      //RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sorted: %d", (std::is_sorted(msg->sortdata.begin(), msg->sortdata.end())));
+      RCLCPP_INFO(this->get_logger(), "Sorted: %d", (std::is_sorted(msg->sortdata.begin(), msg->sortdata.end())));
     }
     
     rclcpp::TimerBase::SharedPtr timer_;
