4920b68af6fc0dd62056d43ec10469d4aeadcad7
Christian Lienen <christianlienen@me.com>
2021-08-13 09:11:14 +0200

    changed arm docker build to ros2 dashing built from source

--- tools/arm-docker-build/install.sh
+++ tools/arm-docker-build/install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo apt install qemu-user-static
-
-cp /usr/bin/qemu-arm-static Docker/ 
-sudo docker image build -t ros_arm:1.0 Docker/
-sudo docker run -it --rm  --name ros_arm_inst -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/Docker/workspace:/mnt/workspace:rw ros_arm:1.0 bash /mnt/workspace/workspace.sh
+sudo docker image build -t ros_arm:2.0 Docker/
+sudo docker run -it --rm  --name ros_arm_inst_2_0 -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/Docker/workspace:/mnt/workspace:rw ros_arm:2.0 bash /mnt/workspace/workspace.sh
