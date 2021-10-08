cabcb173f69f9c7326a41a7ebbd73bdd5a033fb8
Christian Lienen <christianlienen@me.com>
2021-10-08 16:07:54 +0200

    changes on the reconf demo

--- demos/reconf_ros_nodes/src/application/main.c
+++ demos/reconf_ros_nodes/src/application/main.c
@@ -21,6 +21,73 @@
 #include <signal.h>
 
 
+#include "zycap_linux.h"
+
+int reconfigure(char* filename,unsigned int partial){
+	/* construct path of bitfile */
+
+	FILE *bitfile;
+	unsigned int size;
+	char *bitstream;
+
+	bitfile = fopen(filename, "rb");
+	if(!bitfile){
+		printf("Error opening bitfile %s\n",filename);
+		return -1;
+	}
+
+	fseek(bitfile,0L,SEEK_END);
+	size=ftell(bitfile);
+	rewind(bitfile);
+
+	bitstream = (char *)malloc(size*sizeof(char));
+	if(!bitstream){
+		printf("Error allocating memory for bitstream %s\n",filename);
+		return -1;
+	}
+	fread(bitstream, sizeof(char), size, bitfile);
+	fclose(bitfile);
+
+	int fd_partial = open("/sys/devices/soc0/amba/f8007000.devcfg/is_partial_bitstream", O_RDWR);
+	if(fd_partial < 0){
+		printf("Failed to open xdevcfg attribute 'is_partial_bitstream' when configuring %s\n",filename);
+		return -1;
+	}
+
+	char partial_flag[2];
+	if(!partial) {
+		strcpy(partial_flag,"0");
+	}
+	else {
+		strcpy(partial_flag,"1");
+	}
+	write(fd_partial, partial_flag, 2);
+	close(fd_partial);
+
+	fd_partial = open("/dev/xdevcfg", O_RDWR);
+	if(fd_partial < 0){
+		printf("Failed to open xdevcfg device when configuring %s\n",filename);
+		return -1;
+	}
+	//log("Opened xdevcfg. Configuring with %u bytes\n",size);
+	write(fd_partial, bitstream, size);
+	int fd_finish_flag = open("/sys/devices/soc0/amba/f8007000.devcfg/prog_done", O_RDWR);
+	char finish_flag = '0';
+
+	/* wait until reconfiguration is finished */
+	while(finish_flag != '1'){
+		read(fd_finish_flag,&finish_flag,1);
+	}
+	//log("Reconfiguration with bitfile %s finished\n",filename);
+	close(fd_partial);
+	close(fd_finish_flag);
+
+	return 0;
+
+}
+
+
+
 void init_msg(void)
 {
 	//std_msgs__msg__Header header;
@@ -37,10 +104,25 @@ void init_msg(void)
 
 }
 
+static void timespec_diff(struct timespec *start, struct timespec *stop,
+                   struct timespec *result)
+{
+    if ((stop->tv_nsec - start->tv_nsec) < 0) {
+        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
+    } else {
+        result->tv_sec = stop->tv_sec - start->tv_sec;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+    }
+
+    return;
+}
 
 
 int main(int argc, char **argv) {
 
+	struct timespec t_start, t_end, t_res;
+	
 	reconos_init();
 	reconos_app_init();
 
@@ -49,17 +131,27 @@ int main(int argc, char **argv) {
 
 	struct reconos_thread* threads[10];
 
-
 	init_msg();
 
-	threads[0] = reconos_thread_pr_create_hwt_sortdemo(0, "bitstreams");
+	Prefetch_PR_Bitstream("sortdemo");
 
-	threads[1] = reconos_thread_pr_create_hwt_inverse(&inverse_result, "bitstreams");
+	clock_gettime(CLOCK_MONOTONIC, &t_start);
+	Config_PR_Bitstream("sortdemo");
+	clock_gettime(CLOCK_MONOTONIC, &t_end);
+	timespec_diff(&t_start, &t_end, &t_res);
+	printf("%3.6f;\n", (double)(t_res.tv_nsec)/1000000000);
+
+
+
+
+	//threads[0] = reconos_thread_pr_create_hwt_sortdemo(0, "bitstreams");
+
+	//threads[1] = reconos_thread_pr_create_hwt_inverse(&inverse_result, "bitstreams");
 	sleep(5);
 	printf("Suspend block 0 \n");
 	reconos_thread_suspend_block(threads[0]);
 	printf("Start new thread \n");
-	threads[2] = reconos_thread_pr_create_hwt_blur(rsobel_image_msg_out->data.data, "bitstreams");
+	//threads[2] = reconos_thread_pr_create_hwt_blur(rsobel_image_msg_out->data.data, "bitstreams");
 
 	while(1)
 	{
--- demos/reconf_ros_nodes/src/rt_sortdemo/hls/sortdemo.cpp
+++ demos/reconf_ros_nodes/src/rt_sortdemo/hls/sortdemo.cpp
@@ -4,10 +4,22 @@
 #include "ap_int.h"
 #include "ap_fixed.h"
 #include "hls_math.h"
+#include "systemc.h"
 
 #include <sorter_msgs/srv/sort.h>
 
 #define BLOCK_SIZE 2048
+
+#define WAIT_INIT() uint32_t cntr = 0, waitflag = 0;
+
+#define WAIT_HLS(x) {for (int ___i=0; ___i < x; ___i++)	{ \
+		_Pragma("hls pipeline") \
+		_Pragma("hls protocol fixed") \
+		cntr = cntr + 1; \
+		ap_wait(); \
+		if (cntr == x) { \
+			waitflag = 1; \
+		}}}
 
 
 void sort_bubble(uint32_t ram[BLOCK_SIZE]) {
@@ -46,20 +58,29 @@ THREAD_ENTRY() {
 	uint32_t pMessage;
 	uint32_t payload_addr[1];
 
+	WAIT_INIT();
 	THREAD_INIT();
 	initdata = GET_INIT_DATA();
 
 	while(1) {
+		#pragma HLS protocol fixed
+		if(0==ROS_SERVICESERVER_TRYTAKE(rsort_srv, rsort_sort_srv_req, pMessage))
+		{
+			addr = OFFSETOF(sorter_msgs__srv__Sort_Request, unsorted.data) + pMessage;
 
-		pMessage = ROS_SERVICESERVER_TAKE(rsort_srv, rsort_sort_srv_req );
-		addr = OFFSETOF(sorter_msgs__srv__Sort_Request, unsorted.data) + pMessage;
+			MEM_READ(addr, payload_addr, 4);					//Get the address of the data
+			MEM_READ(payload_addr[0], ram, BLOCK_SIZE * 4);
 
-		MEM_READ(addr, payload_addr, 4);					//Get the address of the data
-		MEM_READ(payload_addr[0], ram, BLOCK_SIZE * 4);
-
-		sort_bubble(ram);
-		MEM_WRITE(ram, payload_addr[0], BLOCK_SIZE * 4);
+			sort_bubble(ram);
+			MEM_WRITE(ram, payload_addr[0], BLOCK_SIZE * 4);
 		
-		ROS_SERVICESERVER_SEND_RESPONSE(rsort_srv,rsort_sort_srv_res);
+			ROS_SERVICESERVER_SEND_RESPONSE(rsort_srv,rsort_sort_srv_res);
+		}
+		else
+		{
+			
+			WAIT_HLS(100000);
+		}
+						
 	}
 }
