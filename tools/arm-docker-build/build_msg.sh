39b24195d77ada593840331a5341a21dda69f91a
Christian Lienen <christian.lienen@upb.de>
2021-10-11 15:39:01 +0200

    changed reconros build architecture to support more ros 2 distributions and target platforms

--- tools/arm-docker-build/build_msg.sh
+++ tools/arm-docker-build/build_msg.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker run -it --rm  --name ros_arm_inst -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/:/mnt/project:rw -v $RECONOS:/mnt/reconos:ro ros_arm:1.0 bash /mnt/reconos/tools/arm-docker-build/Docker/workspace/compile_msg.sh
+
+# $1 target architecture (arm32, arm64)
+# $2 target ROS 2 distribution (dashing, foxy)
+
+if [ "$1" = "" || "$2" = "" ]
+then
+  echo "Usage: $0  <arm32,arm64> <dashing,foxy>"
+  exit
+fi
+
+docker run -it --rm  --name reconros_$1_$2_inst_2_0 -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/:/mnt/project:rw -v $RECONOS:/mnt/reconos:ro reconros_$1_$2:2.0 bash /mnt/reconos/tools/arm-docker-build/Docker/workspace/compile_msg.sh
