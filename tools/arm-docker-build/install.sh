8266db7fd86d1e60959ba2f9d7f0b275824007cd
Christian Lienen <christianlienen@me.com>
2021-11-05 11:37:59 +0100

    prevent using caches for docker image building

--- tools/arm-docker-build/install.sh
+++ tools/arm-docker-build/install.sh
@@ -18,5 +18,5 @@ fi
 
 
 cp Docker/qemu-arm-static Docker/$1_$2/
-docker image build -t reconros_$1_$2:2.0 Docker/$1_$2/
+docker image build -t reconros_$1_$2:2.0 Docker/$1_$2/ --no-cache
 docker run -it --rm  --name reconros_$1_$2_inst_2_0 -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/Docker/workspace:/mnt/workspace:rw reconros_$1_$2:2.0 bash /mnt/workspace/workspace.sh
