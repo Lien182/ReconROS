874117bcf7b813efd469a02918e503297430a7ea
shm117 <sorel.middeke@googlemail.com>
2022-09-01 14:06:56 +0200

    publish message to hwtopic from thread b

--- demos/ros_hwtopic_demo/server_app/src/rt_athreadb/hls/rt_threadb.cpp
+++ demos/ros_hwtopic_demo/server_app/src/rt_athreadb/hls/rt_threadb.cpp
@@ -21,26 +21,107 @@ THREAD_ENTRY() {
 	uint64_t addr, initdata;	
 	uint64_t pMessage;
 	uint64_t payload_addr[1];
+	uint64_t payload[1];
 
 	sensor_msgs__msg__Image image_msg;
+
+	uint8_t image_data[200];
+	char encoding[5];
+	char frame_id[10];
+
+	image_msg.data.data = image_data;
+	image_msg.encoding.data = encoding;
+	image_msg.header.frame_id.data = frame_id;
 
 	THREAD_INIT();
 	initdata = GET_INIT_DATA();
 
-
 	uint32_t ram[64];
-	uint32_t address_offset = 0;
+	uint64_t address_offset = 0;
 
 	while(1) {
 
 		// HWThread b: subscribe to data from software-domain, publish to hwtopic
 
 		uint64_t msg = ROS_SUBSCRIBE_TAKE(rthreadb_subdata, rthreadb_img_input);
-		MEM_READ(msg, payload_addr, 8);
+
+		MEM_READ(msg, payload, MEM_STEP);
+		image_msg.header.stamp.sec = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.header.stamp.nanosec = payload[0];
+		address_offset += MEM_STEP;
+
+		// frame_id string
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.header.frame_id.size = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.header.frame_id.capacity = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload_addr, MEM_STEP);
+		MEM_READ(payload_addr[0], image_msg.header.frame_id.data, MEM_STEP * FRAME_ID_SIZE);
+		address_offset += MEM_STEP;
+		//
+
+
+		MEM_READ(msg, payload, MEM_STEP);
+		image_msg.height = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.width = payload[0];
+		address_offset += MEM_STEP;
+
+
+		// encoding string
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.encoding.size = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.encoding.capacity = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload_addr, MEM_STEP);
+		MEM_READ(payload_addr[0], image_msg.encoding.data, MEM_STEP * ENCODING_SIZE);
+		address_offset += MEM_STEP;
+		//
+
+		MEM_READ(msg, payload, MEM_STEP);
+		image_msg.is_bigendian = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.step = payload[0];
+		address_offset += MEM_STEP;
+
+
+		// data array
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.data.size = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload, MEM_STEP);
+		image_msg.data.capacity = payload[0];
+		address_offset += MEM_STEP;
+
+		MEM_READ(msg + address_offset, payload_addr, MEM_STEP);
+		MEM_READ(payload_addr[0], image_msg.data.data, MEM_STEP * DATA_SIZE);
+		address_offset += MEM_STEP;
+		//
+
+		ROS_PUBLISH_HWTOPIC_nicehwtopic(nicehwtopic, &image_msg);
+
+
+		/*
 		ap_axis<32,1,1,1> tmp_frame;
 		tmp_frame.data = payload_addr[0]; 
 		nicehwtopic.write(tmp_frame);
-
+		*/
 	}
 }
 
