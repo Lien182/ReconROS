95990b07522d4a5f1d1aad949221798032de76ca
Christian Lienen <christian.lienen@upb.de>
2023-03-20 14:40:22 +0100

    minor changes

--- templates/thread_hls_build/script_csynth.tcl
+++ templates/thread_hls_build/script_csynth.tcl
@@ -2,9 +2,9 @@
 
 open_project hls
 set_top rt_imp
-add_files reconos_calls.h
-add_files reconos_thread.h
-add_files [ glob *.cpp ] -cflags "-I/opt/ros/<<ROSDISTRIBUTION>>/include/ <<MSGINCLUDEDIR>> -m<<CPUARCHITECTURESIZE>>"
+add_files reconos_calls.h -cflags "-m<<CPUARCHITECTURESIZE>>"
+add_files reconos_thread.h -cflags "-m<<CPUARCHITECTURESIZE>>"
+add_files [ glob *.cpp ] -cflags "-I/opt/ros/<<ROSDISTRIBUTION>>/include/ -m<<CPUARCHITECTURESIZE>> <<MSGINCLUDEDIR>>"
 open_solution sol
 set_part {<<PART>>}
 create_clock -period <<CLKPRD>> -name default
